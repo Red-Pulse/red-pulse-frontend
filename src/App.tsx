95ae918b3f8ae494dcbdf6c2a41c3657e9831100
aza <aza.savkimov@gmail.com>
2024-04-14 21:17:01 +0500

    feat: ui button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import UITypography from './components/UI/UITypography';
+import UITypography from 'components/UI/UITypography';
+import UIButton from 'components/UI/UIButton';
 
 function App() {
-
   return (
-    <UITypography typography={'text_fz56_lh72'} color={'red'}>Hello</UITypography>
+    <div>
+      <UITypography typography="text_fz56_lh72" color="red">
+        Hello
+      </UITypography>
+      <UIButton size="small">Login</UIButton>
+      <UIButton size="medium" variant="primary">
+        Login
+      </UIButton>
+      <UIButton size="large" disabled>
+        Login
+      </UIButton>
+    </div>
   );
 }
 
--- src/components/UI/UITypography/UITypography.tsx
+++ src/components/UI/UITypography/UITypography.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { typographyValues } from '../typography.ts';
+import { typographyValues } from './typography.ts';
+import { colors } from 'assets/colors.ts';
 
 interface TypographyProps {
   typography: keyof typeof typographyValues;
-  color: string;
-  children: React.ReactNode
+  color?: keyof typeof colors;
+  children: React.ReactNode;
 }
 
-const UITypography: React.FC<TypographyProps> = ({typography, color ,children}) => {
+const UITypography: React.FC<TypographyProps> = ({
+  typography,
+  color = 'textBlack100',
+  children,
+}) => {
   const typographyValue = typographyValues[typography];
   return (
     <div
       style={{
         fontFamily: 'Roboto',
         fontSize: typographyValue.fontSize,
-        fontWeight:typographyValue.lineHeight,
-        color,
+        fontWeight: typographyValue.lineHeight,
+        color: colors[color],
       }}
     >
       {children}
